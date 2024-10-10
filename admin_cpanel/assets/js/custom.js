401a3db2e7383fa8440d00f1612fb33b03068e74
Khladyn <jeankhladyn.abarquez.iics@ust.edu.ph>
2024-10-10 20:04:59 +0800

    Initial commit with no history.

---
+++ admin_cpanel/assets/js/custom.js
@@ -0,0 +1,35 @@
+/* Navigation */
+document.querySelectorAll(".orca-main-nav .nav-link").forEach((link) => {
+    if (link.href === window.location.href) {
+        link.classList.add("active");
+        link.setAttribute("aria-current", "page");
+    }
+});
+
+/* Back To Top */
+let mybutton = document.getElementById("back-to-top");
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+const qna_tags_create = UseBootstrapTag(document.getElementById('qna_tags_create'));
+const qna_tags_edit = UseBootstrapTag(document.getElementById('qna_tags_edit'));
